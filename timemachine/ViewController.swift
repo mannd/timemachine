ffcec12b6c94a15120ee0cedf9017c0668db6120
David Mann <mannd@epstudiossoftware.com>
2016-06-17 21:58:31 -0600

    Add origin date/time
    Add timer, add destination labels.

--- timemachine/ViewController.swift
+++ timemachine/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var originTimeLabel: UILabel!
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerDidTick), userInfo: nil, repeats: true)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func timerDidTick() {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy MMM d HH:mm:ss.SS"
+        originTimeLabel.text = (dateFormatter.string(from: date))
     }
 
 
