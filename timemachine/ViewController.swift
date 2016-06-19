8b4dac7bdb48e8818c3a156197968c39b0ec743a
David Mann <mannd@epstudiossoftware.com>
2016-06-19 15:32:26 -0600

    Change UI
    Autolayout works for portrait modes.  Landscape modes overlap.

--- timemachine/ViewController.swift
+++ timemachine/ViewController.swift
@@ -12,13 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var originTimeLabel: UILabel!
     var timer: Timer?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerDidTick), userInfo: nil, repeats: true)
-        
-        
+         
     }
 
     override func didReceiveMemoryWarning() {
